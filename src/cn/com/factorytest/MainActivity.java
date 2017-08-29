75d6f66d67eea3e0e952db161eabb70fe9bec093
terry <terry@szwesion.com>
2017-08-29 18:36:01 +0800

    add button for disabling wol function

--- src/cn/com/factorytest/MainActivity.java
+++ src/cn/com/factorytest/MainActivity.java
@@ -542,6 +542,15 @@ private void updateEthandWifi(){
 		}
 	}
 
+	public void DisableWol(View view){
+		Log.e(TAG, "DisableWol");
+		try {
+			Process proc = Runtime.getRuntime().exec(new String[]{"su","-c","i2cset -f -y 2 0x18 0x21 0x0"});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
    public void KeyTest(View view){
          Log.e(TAG, "KeyTest()");
 
